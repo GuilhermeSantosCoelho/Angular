a89111daef868b2d4cdfed71940f43380df69405
Guilherme Santos Coelho <guilherme.santos@concert.com.br>
2020-11-11 12:33:19 -0300

    Event binding

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -10,12 +10,31 @@ export class CursosComponent implements OnInit {
 
   cursos: string[];
   imagemUrl: string = "http://lorempixel.com/400/200/sports";
+  valorAtual: string = '';
+  valorSalvo: string = '';
+  isMouseOver: boolean = false;
 
   constructor(private cursosService: CursosService) {
     this.cursos = this.cursosService.getCursos();
   }
 
+  botaoClicado(){
+    alert('Botão clicado.');
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor){
+    this.valorSalvo = valor;
+  }
+
   ngOnInit(): void {
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
   }
 
 }
