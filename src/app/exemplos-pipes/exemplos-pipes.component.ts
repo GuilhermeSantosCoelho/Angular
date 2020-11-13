639d2f57b78a698456566c0c152028da037d6246
Guilherme Santos Coelho <guilherme.santos@concert.com.br>
2020-11-13 13:51:18 -0300

    Pipes async

--- src/app/exemplos-pipes/exemplos-pipes.component.ts
+++ src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-exemplos-pipes',
@@ -19,6 +21,13 @@ export class ExemplosPipesComponent implements OnInit {
   livros: string[] = ['Angular', 'Java'];
 
   filtro: string = '';
+
+  valorAsync = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Valor assíncrono'), 2000)
+  })
+
+  valorAsync2 = interval(2000)
+    .pipe(map(valor => 'Valor assíncrono 2'))
 
   constructor() { }
 
