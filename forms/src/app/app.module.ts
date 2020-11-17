16a5ac05b852f921c518c928f36af049b1a14bba
Guilherme Santos Coelho <guilherme.santos@concert.com.br>
2020-11-17 14:35:23 -0300

    Forms (template driven) Módulos e FormsModule

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -6,16 +6,14 @@ import { AppComponent } from './app.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { TemplateFormComponent } from './template-form/template-form.component';
 import { DataFormComponent } from './data-form/data-form.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
+import { TemplateFormModule } from './template-form/template-form.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateFormComponent,
     DataFormComponent
   ],
   imports: [
@@ -24,9 +22,8 @@ import { FormsModule } from '@angular/forms';
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatFormFieldModule,
     MatInputModule,
-    FormsModule
+    TemplateFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
