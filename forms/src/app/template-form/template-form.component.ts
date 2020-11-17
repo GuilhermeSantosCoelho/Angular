acac1dd8a196d2bf524d78830f6b0d5420c312e8
Guilherme Santos Coelho <guilherme.santos@concert.com.br>
2020-11-17 14:18:59 -0300

    Forms (template driven) Inicializando valores com ngModel

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateFormComponent implements OnInit {
 
+  usuario: any = {
+    nome: 'Guilherme',
+    email: 'guilherme@gmail.com'
+  }
+
   constructor() { }
 
   ngOnInit(): void {
