ecf05f3b92fb9769cb25a10070fdfc5147fd4d8c
Guilherme Santos Coelho <guilherme.santos@concert.com.br>
2020-11-18 16:34:04 -0300

    Formulários reativos: Configuração (Módulo e Componente)

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -6,15 +6,14 @@ import { AppComponent } from './app.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { DataFormComponent } from './data-form/data-form.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { TemplateFormModule } from './template-form/template-form.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DataFormModule } from './data-form/data-form.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataFormComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +22,9 @@ import { TemplateFormModule } from './template-form/template-form.module';
     MatButtonModule,
     MatIconModule,
     MatInputModule,
-    TemplateFormModule
+    TemplateFormModule,
+    ReactiveFormsModule,
+    DataFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
